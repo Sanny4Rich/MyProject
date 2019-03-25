d553b43d62acea884b0f0d4ca3384030b135d122
Oleksandr Hrachov <sanny4rich@gmail.com>
2019-03-25 12:31:23 +0200

    Вывод дерева категорий

--- src/Controller/CategoryBanerController.php
+++ src/Controller/CategoryBanerController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\CategoriesRepository;
-use App\Repository\ProductRepository;
-use App\Service\GetNamesFromRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,38 +11,57 @@ class CategoryBanerController extends AbstractController
     /**
      * @Route("/category/baner", name="category_baner")
      */
-    public function index(ProductRepository $productRepository, CategoriesRepository $categoryRepository)
+    //public function index(ProductRepository $productRepository, CategoriesRepository $categoryRepository)
+    public function index(CategoriesRepository $categoryRepository)
     {
+        $categories = $categoryRepository->findAll();
+//        $categories = $this->prepareCategories();
+        var_dump($categories);
+        return $this->render('categoryBaner/index2.html.twig', [
+            'categories' => $categories,
+        ]);
 
-        $products = $productRepository->findBy(
-            ['isTop' => true],
-            ['name' =>'ASC']);
-        $controller = $this;
-        $options = array('decorate'=>true,
-            'rootOpen'=> '<br>',
-            'rootClose' => '<br>',
-            'childOpen' => '<br>',
-            'childClose' => '<br>',
-            'representationField' => 'name',
-            'html' => true,
-            'nodeDecorator' => function($node) use ($controller) {
-                $url = $controller->generateUrl('category_item', ['id' => $node['id']]);
-                return '<a href="' . $url . '">' . $node['name'] . '</a>';});
-
-        $category = new GetNamesFromRepository($category);
-        $tree = $categoryRepository->childrenHierarchy(
-             $categoryRepository->findOneBy($category),
-            null,
-            $options
-        );
-        dump($tree);
+//        $controller = $this;
+//        $options = array('decorate'=>true,
+//            'rootOpen'=> '<br>',
+//            'rootClose' => '<br>',
+//            'childOpen' => '<br>',
+//            'childClose' => '<br>',
+//            'representationField' => 'name',
+//            'html' => true,
+//            'nodeDecorator' => function($node) use ($controller) {
+//                $url = $controller->generateUrl('category_item', ['id' => $node['id']]);
+//                return '<a href="' . $url . '">' . $node['name'] . '</a>';});
+//        $alltree = $categoryRepository->findAll();
+//
+//        $tree = $categoryRepository->childrenHierarchy(null,
+//            null,
+//            $options
+//        );
 
         return $this->render('categoryBaner/index.html.twig', [
             'products' => $products,
             'tree' => $tree,
-            'name' => $name,
-            ''
         ]);
+    }
+
+    private function prepareCategorie($categories): array
+    {
+        $result = [
+            1 => [
+                'name' => 'test 1',
+                'childrens' => [
+                    2 => [
+                        'name test 1.1'
+                    ],
+                ],
+            ],
+            12 => [
+
+            ],
+        ];
+
+        return $result;
     }
 }
 
--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -15,40 +15,12 @@ class HomePageController extends AbstractController
      */
     public function index(ProductRepository $productRepository, CategoriesRepository $categoryRepository)
     {
-        $categoryOnMainPage =    array(['name'=>"Books & Board Games"],
-            ['name'=>"Baby Dolls"],
-            ['name'=>"Cameras"],
-            ['name'=>"GamePad"],
-            ['name'=>"Health & Beauty"],
-            ['name'=>"Makeup"],
-            ['name'=>"Fruits"],
-            ['name'=>"Vegetables"]);
-
-        $products = $productRepository->findBy(
-            ['isTop' => true],
-            ['name' =>'ASC']);
-        $controller = $this;
-        $options = array('decorate'=>true,
-            'rootOpen'=> '<br>',
-            'rootClose' => '<br>',
-            'childOpen' => '<br>',
-            'childClose' => '<br>',
-            'representationField' => 'name',
-            'html' => true,
-            'nodeDecorator' => function($node) use ($controller) {
-                $url = $controller->generateUrl('category_item', ['id' => $node['id']]);
-                return '<a href="' . $url . '">' . $node['name'] . '</a>';});
-
-        $category = array();
-        $tree = $categoryRepository->childrenHierarchy(
-            $categoryRepository->findOneBy($category),
-            null,
-            $options
-        );
-
+        $categories = $categoryRepository->findAll();
+//        $categories = $this->prepareCategories();
+//        var_dump($categories);
             return $this->render('home_page/index.html.twig', [
-                'products' => $products,
-                'tree' => $tree,
+                'categories' => $categories,
+
         ]);
     }
 
